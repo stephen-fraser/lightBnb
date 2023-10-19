67c84ea2f61e1ed3fd589d07cf92ad1efeaf45cf
stephen-fraser <steve.james.fraser@gmail.com>
2023-10-19 16:39:16 -0700

    add return to insert of new properties

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -209,14 +209,14 @@ const addProperty = function (property) {
 
   const queryString = `
     INSERT INTO properties (owner_id,title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14);
-  `;
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;
+  ` ;
 
   return pool
     .query(queryString, values)
     .then((result) => {
-      console.log(result.rows);
-      return result.rows;
+      console.log(result.rows[0]);
+      return result.rows[0];
     })
     .catch((err) => {
       console.log(err.message);
